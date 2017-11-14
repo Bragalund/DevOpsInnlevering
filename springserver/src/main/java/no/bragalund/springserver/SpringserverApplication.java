892541263b230aca647b3447f7da9a52cded915c
Bragalund <bakhen15@student.westerdals.no>
2017-11-14 16:20:29 +0100

    Created react app

---
+++ springserver/src/main/java/no/bragalund/springserver/SpringserverApplication.java
@@ -0,0 +1,12 @@
+package no.bragalund.springserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringserverApplication.class, args);
+	}
+}
